ee72020916101b423acee9e037e112ff31dafb0b
Aviv Bueno <aviv.buen@gmail.com>
2017-09-23 02:40:31 +0300

    456

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html', function(err, data) {
+  fs.readFile('public/index.html', function(err, data) {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(data);
     response.end();
