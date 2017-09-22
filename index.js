a3c264c41b828b6e99b4fc765a10135d11f461f8
Aviv Bueno <aviv.buen@gmail.com>
2017-09-23 02:34:04 +0300

    1

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-var express = require('express');
-var app = express();
-var formidable = require('formidable');
-var fs = require('fs');
+var express = require('express')
+var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  if (request.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(request, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        response.write('File uploaded and moved!');
-        response.end();
-      });
-      });
-  } else {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    response.write('<input type="file" name="filetoupload"><br>');
-    response.write('<input type="submit">');
-    response.write('</form>');
-    return response.end();
-  }
+app.get('/', function(request, res) {
+    fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 })
 
 app.listen(app.get('port'), function() {
