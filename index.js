03cbc75ec51459be5537b5e011e7eba2220d4fa3
Aviv Bueno <aviv.buen@gmail.com>
2017-09-23 02:20:50 +0300

    Test230920170202

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-var express = require('express')
-var app = express()
-var fs = require('fs')
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-    fs.readFile('index.html', function(err, data) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(data);
-    response.end();
-  });
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
