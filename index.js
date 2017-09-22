3eeeb6ffa348471e104df3772ea279cf7d47e0f5
Aviv Bueno <aviv.buen@gmail.com>
2017-09-23 02:36:20 +0300

    321

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function(request, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 })
 
 app.listen(app.get('port'), function() {
