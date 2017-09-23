5bbca7f1ce27117f2eaaf3615480f5f44a3ce40d
Aviv Bueno <aviv.buen@gmail.com>
2017-09-23 13:31:39 +0300

    089

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
+var url = require('url')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+app.get('/m8', function(request, response) {
+  var q = url.parse(request.url, true).query;
+  res.end(q);
 })
 
 app.listen(app.get('port'), function() {
