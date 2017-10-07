af4b6e3cc5e766db85c751953d2ff49706c6be7b
Aviv Bueno <aviv.buen@gmail.com>
2017-10-07 15:46:50 +0300

    test45

--- public/app.js
+++ public/app.js
@@ -1,6 +1,14 @@
 var socket = io.connect("https://nodejs-aviv.herokuapp.com");
+var app = new Vue({
+    el: '#app',
+    data: {
+        concode:'000000'
+    },
+    methods: {
+    }
+});
+
 
 socket.on('acmedia',function(data){
-    Materialize.toast(data);
-
+    //Data handle
 });
