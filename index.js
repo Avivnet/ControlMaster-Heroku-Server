73f2e1436a0bb30ec92c6fc7ddf5f3e198fef9f5
Aviv Bueno <aviv.buen@gmail.com>
2017-10-07 15:52:22 +0300

    11

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ var app  = express();
 var server = app.listen((process.env.PORT || 5000),function(){
     console.log("Server UP ON PORT: "+ (process.env.PORT || 5000));
 });
-
+app.get("/newcode",function(req,res){
+    res.write(getRandomInt(100000,1000000));
+});
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
 // Static Routes
 app.use(express.static('public'));
+
 
 // Socket Setup
 var io = socket(server);
