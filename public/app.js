2f0cf69fb20d2dc5031877b4aec847757f158243
Aviv Bueno <aviv.buen@gmail.com>
2017-10-11 18:24:24 +0300

    Nopostback fix

--- public/app.js
+++ public/app.js
@@ -5,17 +5,24 @@ var app = new Vue({
         concode:'000000'
     },
     methods: {
-        
+        newCode: function(){
+            $("#code-section").hide();
+            $("#codepre").show();
+            socket.disconnect();
+            socket.connect();
+        }
     },
     mounted() { // when the Vue app is booted up, this is run automatically.
        
-        
     }
 });
 
 
 socket.on('concode',function(data){
     app.concode=data.toString();
+    $("#codepre").hide();
+    $("#code-section").fadeIn();
+
 });
 socket.on('acmedia',function(data){
     //Data handle
