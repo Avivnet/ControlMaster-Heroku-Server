e32c4950a32429928e02dbdeda0958f346924d4a
avivbuen <aviv.buen@gmail.com>
2017-10-19 02:22:55 +0300

    itzuv

--- public/app.js
+++ public/app.js
@@ -13,10 +13,35 @@ var app = new Vue({
         }
     },
     mounted() { // when the Vue app is booted up, this is run automatically.
-       
+        $('.modal').modal();
+        if(getCookie('firstTime')=="")
+        {
+            $("#getstarted").modal('open');
+            setCookie('firstTime','the_value', 365);
+        }
     }
 });
-
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 socket.on('concode',function(data){
     app.concode=data.toString();
