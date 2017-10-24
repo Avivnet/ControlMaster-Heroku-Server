2ecfe87dc3db67819f252655eadb7ee1a5cd7c1e
avivbuen <aviv.buen@gmail.com>
2017-10-24 17:50:59 +0300

    23q321

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ var app  = express();
 var server = app.listen((process.env.PORT || 5000),function(){
     console.log("Server UP ON PORT: "+ (process.env.PORT || 5000));
 });
+
+//Get all active connections
 app.get('/connections', function (req, res) {
     res.send(connections);
 });
+
+/*
 app.get('/color/:r/:g/:b',function(req,res){
     var rgb = [req.params.r,req.params.g,req.params.b];
     for(var i = 0; i<rgb.length; i++)
         if(isNaN(rgb[i])) res.send('wrong parameters!'); return;
     var color = 'rgb('+req.params.r+','+req.params.g+','+req.params.b+')';
     res.send(color);
-
 });
+*/
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -30,7 +35,7 @@ var io = socket(server);
 
 io.on('connection',function(socket){
     //Start a new connection
-    var connection = {connectionCode:getNewCode(),socketid:socket.id, phoneIp:socket.handshake.address}
+    var connection = {connectionCode:getNewCode(),socketid:socket.id,c_socketid:0, phoneIp:socket.handshake.address}
     //Add the connection to the list
     connections.push(connection);
     //Send the connection code back to the newly connected client
@@ -39,8 +44,20 @@ io.on('connection',function(socket){
     console.log("WS Connected - "+ JSON.stringify(connection));
     //Handling an action of button click on the phone
     socket.on('acmedia', function(data){
-        
-        io.sockets.emit('acmedia',data);
+        if(connection.c_socketid!=0)
+            io.sockets.connected(connection.c_socketid).emit('acmedia',data);
+    });
+    //The computer says im a computer and i have a code
+    socket.on('im_comp',function(data){
+        var c_index = find(connectionCode);
+        if(c_index!=-1){
+            remove(connections,find(socket.id,true));
+            connection = connection[c_index];
+            connection.c_socketid= socket.id;
+        }
+        else{
+            socket.emit('nsc');
+        }
     });
     socket.on("disconnect",function(){
         remove(connections,find(socket.id,true));
